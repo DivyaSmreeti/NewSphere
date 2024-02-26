35d907d67ba1252392b71f78b9184b732cdc7932
Divya <divyasmreeti@gmail.com>
2024-02-26 17:21:36 +0530

    third commit

--- HomePage/home.js
+++ HomePage/home.js
@@ -16,66 +16,32 @@ $(document).ready(function () {
 });
 
 
-  $(document).ready(function () {
-    // Load existing posts from local storage when the page loads
-    loadPosts();
+/* for sidebar navigation */
 
-    // Show the modal when the "New Post" link is clicked
-    $('#newPostLink').on('click', function () {
-      $('#newPostModal').modal('show');
-    });
 
-    // Handle form submission
-    $('#newPostForm').submit(function (event) {
-      event.preventDefault();
 
-      // Get the content from the textarea
-      var postContent = $('#postContent').val();
+function showpage1()
+{
+    document.getElementById("page1Content").style.display="block";
+    document.getElementById("page2Content").style.display="none";
+}
+function showpage2()
+{
+    document.getElementById("page1Content").style.display="none";
+    document.getElementById("page2Content").style.display="block";
+    
+}
 
-      // Append the content to the content area
-      addPost(postContent);
 
-      // Close the modal
-      $('#newPostModal').modal('hide');
+document.querySelector('#details').addEventListener('click', function () {
+    showpage1();
+});
 
-      // Clear the form
-      $('#newPostForm')[0].reset();
-    });
+document.querySelector('#calendar').addEventListener('click', function () {
+    showpage2();
+});
 
-    // Handle post deletion
-    $('#content').on('click', '.delete-post', function () {
-      var postIndex = $(this).data('index');
-      deletePost(postIndex);
-    });
 
-    // Function to load posts from local storage
-    function loadPosts() {
-      var posts = JSON.parse(localStorage.getItem('posts')) || [];
-      for (var i = 0; i < posts.length; i++) {
-        $('#content').append('<div class="line"></div><p>' + posts[i] + ' <button class="delete-post" data-index="' + i + '">Delete</button></p>');
-      }
-    }
 
-    // Function to add a post to local storage and display it
-    function addPost(postContent) {
-      var posts = JSON.parse(localStorage.getItem('posts')) || [];
-      posts.push(postContent);
-      localStorage.setItem('posts', JSON.stringify(posts));
-      $('#content').append('<div class="line"></div><p>' + postContent + ' <button class="delete-post" data-index="' + (posts.length - 1) + '">Delete</button></p>');
-    }
 
-    // Function to delete a post from local storage and remove it from display
-    // function deletePost(index) {
-    //   var posts = JSON.parse(localStorage.getItem('posts')) || [];
-    //   posts.splice(index, 1);
-    //   localStorage.setItem('posts', JSON.stringify(posts));
-    //   refreshPosts();
-    // }
-
-    // Function to refresh posts on the page
-    function refreshPosts() {
-      $('#content').empty();
-      loadPosts();
-    }
-  });
-
+/* sidebar navigation ends */
